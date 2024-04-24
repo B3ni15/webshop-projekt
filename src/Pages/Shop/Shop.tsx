d6a75698a5af58cc2972afe433c412cdb5371b5c
B3ni <90302291+B3ni15@users.noreply.github.com>
2024-04-24 08:41:22 +0200

    Update text styles in Shop.tsx component

--- src/Pages/Shop/Shop.tsx
+++ src/Pages/Shop/Shop.tsx
@@ -12,12 +12,11 @@ const Shop = () => {
                 <img src="/rect24.png" alt="Logo" />
 
                 <p className='flex flex-col text-center'>
-                    <span className='text-green-600 p-1'>A Speed Ball boltban mindig pezseg az élet, ahogy az emberek rohannak be-ki.</span>
-                    <span className='text-sky-600 p-1'>Az illatok és színek kavalkádja tölti be a Speed Ball boltot.</span>
-                    <span className='text-red-600 p-1'>A polcokon sorakoznak az energiával teli italok és gyorsan fogyasztható ételek.</span>
+                    <span className='font-medium text-xl'>A Speed Ball boltban mindig pezseg az élet, ahogy az emberek rohannak be-ki.</span>
+                    <span className='font-semibold text-xl'>Az illatok és színek kavalkádja tölti be a Speed Ball boltot.</span>
+                    <span className='font-bold text-xl'>A polcokon sorakoznak az energiával teli italok és gyorsan fogyasztható ételek.</span>
                 </p>
 
-                {/* Csak akkor jelenítjük meg a gombot, ha nem a /shop oldalon vagyunk */}
                 {!isShopPage &&
                     <Link to="/shop" className="flex gap-2 items-center justify-center p-2 px-6 rounded-full from-[#0085FF] to-[#000699] bg-gradient-to-r text-white">
                         <img src="/kisicon.png" alt="Kisicon" className='w-6' />
